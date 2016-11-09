a12731c865c8c14dc5e9552d4876b87791de6ddf
Michael <Michael@DESKTOP-AA4T0FR>
2016-11-09 18:21:30 -0500

    Add project files.

---
+++ FileToBinary/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace FileToBinary {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.FileSelect = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // FileSelect
+            // 
+            this.FileSelect.Location = new System.Drawing.Point(75, 100);
+            this.FileSelect.Name = "FileSelect";
+            this.FileSelect.Size = new System.Drawing.Size(106, 30);
+            this.FileSelect.TabIndex = 0;
+            this.FileSelect.Text = "Select File";
+            this.FileSelect.UseVisualStyleBackColor = true;
+            this.FileSelect.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 28);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(241, 66);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "This program takes files of any type and converts it to a binary file";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(280, 187);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.FileSelect);
+            this.Name = "Form1";
+            this.Text = "Files To Binary";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button FileSelect;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ FileToBinary/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileToBinary {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e){
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+
+            // Variables
+            DialogResult result;
+            string filename;
+            string extension;
+
+            //  Uses Open FileDialog to get a file
+            using (var fileChooser = openFileDialog1) {
+                fileChooser.FileName = "";
+                result = fileChooser.ShowDialog();
+                filename = fileChooser.FileName;
+            }
+
+            // Opens a fileDialog
+            filename = openFileDialog1.FileName;
+            extension = Path.GetExtension(filename);
+
+
+            // Bit Array for the file
+            BitArray bits = GetBinaryFile(filename);
+
+            // Save File Dialog Popup
+            saveFileDialog1.ShowDialog();
+            string pathName = saveFileDialog1.FileName.ToString();
+            
+            // Writes the bytes to the new selected file
+            Stream stream = new FileStream(pathName, FileMode.Create);
+            foreach(Boolean b in bits) {
+                stream.WriteByte(Convert.ToByte(b));
+            }           
+            stream.Close(); //closes stream
+
+
+            MessageBox.Show(filename + " bytes have been stored in " + pathName);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) {
+
+        }
+
+        /// <summary>
+        /// Gets a bit array (Collections) from a specific file
+        /// http://stackoverflow.com/questions/24106687/convert-file-to-binary-in-c-sharp
+        /// </summary>
+        /// <param name=""></param>
+        /// <returns></returns>
+        private BitArray GetBinaryFile(string filename) {
+
+            byte[] bytes;
+            using (FileStream file = new FileStream(filename, FileMode.Open, FileAccess.Read)) {
+                bytes = new byte[file.Length];
+                file.Read(bytes, 0, (int)file.Length);
+            }
+
+            return new BitArray(bytes);
+        }
+    }
+}
